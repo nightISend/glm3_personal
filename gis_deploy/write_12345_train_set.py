82d90f1bdaa6793cabfb57cdd2b0828380adac6a
燦 <422032918@qq.com>
2025-01-11 09:03:51 +0800

    修改了数据集

--- gis_deploy/write_12345_train_set.py
+++ gis_deploy/write_12345_train_set.py
@@ -3,13 +3,13 @@ import json
 def write_set(data):
     # 标注utf-8否则乱码
     file=open("data_set.json",'w',encoding="utf-8")
-    content="任务:请从给出的关于海宁市的文本中提取事件的发生地，发生地必须是海宁市内的某个地点，不可以指海宁市这一大区域，如果没有发现位置信息则返回无位置信息。"
-    data_dict={"instruction":content,"input":"","output":""}
+    data_dict={"instruction":"","input":"","output":""}
     data_dict_list=[]
     for row in range(len(data)):
         true_address=data.loc[row,'true_address']
         events=data.loc[row,'events']
-        data_dict["input"]="文本:"+events
+        content=f'''任务:请从给出的关于海宁市的文本中提取事件的发生地，发生地必须是海宁市内的某个地点，不可以指海宁市这一大区域，如果没有发现位置信息则返回无位置信息。\n文本:{events}'''
+        data_dict["input"]=content
         data_dict["output"]="事件发生地:"+true_address
         # 字典是指针，前面存入的字段与后面的字典是相同的地址
         example=data_dict.copy()
